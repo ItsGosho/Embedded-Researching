c8399732e8d5b582626f885eabda368c13617743
itsgosho <itsgosho2@gmail.com>
2021-07-20 18:51:30 +0300

    Abstracted the ASCII Finder as Sequence Finder to work with all various of data types.

--- Experimental/Serial-Manipulation/PC/src/main/java/ArduinoSerialCommunication.java
+++ Experimental/Serial-Manipulation/PC/src/main/java/ArduinoSerialCommunication.java
@@ -38,40 +38,6 @@ public class ArduinoSerialCommunication {
     }
 
     /**
-     * Responsible for finding a ASCII Sequence.
-     */
-    private class ASCIISequence {
-
-        private List<Integer> sequence;
-        private List<Integer> expected;
-
-        public ASCIISequence(Integer... characterCodes) {
-            this.expected = Arrays.asList(characterCodes);
-            this.sequence = new ArrayList<>();
-        }
-
-        /**
-         * Will insert character to the current sequence.
-         * If the character fails the pattern, then the current sequence will be reset.
-         *
-         * @return true/false depending if the sequence was completed
-         */
-        public boolean insert(int characterCode) {
-
-            this.sequence.add(characterCode);
-
-            Integer expectedAtPosition = this.expected.get(this.sequence.size() - 1);
-
-            if (!expectedAtPosition.equals(characterCode)) {
-                this.sequence.clear();
-                return false;
-            }
-
-            return this.sequence.size() == this.expected.size();
-        }
-    }
-
-    /**
      * Will finish, when the SYN character is received.
      * It is indication that the embedded device has started
      *
@@ -79,12 +45,12 @@ public class ArduinoSerialCommunication {
      */
     private boolean synchronizeNew() {
         StopWatch stopWatch = StopWatch.createStarted();
-        ASCIISequence asciiSequence = new ASCIISequence(22, 13, 10);
+        SequenceFinder<Integer> sequenceFinder = new SequenceFinder<>(22, 13, 10);
 
         while (true) {
-            Byte value = this.readByte();
+            int value = this.readByte();
 
-            if (asciiSequence.insert(value)) {
+            if (sequenceFinder.insert(value)) {
                 System.out.println("Synchronization finished in " + stopWatch.toString());
                 stopWatch.stop();
                 return true;
--- Experimental/Serial-Manipulation/PC/src/main/java/Main.java
+++ Experimental/Serial-Manipulation/PC/src/main/java/Main.java
@@ -10,9 +10,9 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
 
         ArduinoSerialCommunication serialCommunication = new ArduinoSerialCommunication();
-        serialCommunication.start();
+        //serialCommunication.start();
 
-    /*    ASCIISequence asciiSequence = new ASCIISequence();
+        SequenceFinder<Integer> asciiSequence = new SequenceFinder<>(22, 13, 14);
 
 
         List<Integer> characterTests = new ArrayList<>();
@@ -29,41 +29,7 @@ public class Main {
                 System.out.println("FOUND!");
                 break;
             }
-        }*/
-
-    }
-
-    /**
-     * Responsible for finding a ASCII Sequence.
-     */
-    private static class ASCIISequence {
-
-        private List<Integer> sequence;
-        private List<Integer> expected;
-
-        public ASCIISequence(Integer... characterCodes) {
-            this.expected = Arrays.asList(characterCodes);
-            this.sequence = new ArrayList<>();
         }
 
-        /**
-         * Will insert character to the current sequence.
-         * If the character fails the pattern, then the current sequence will be reset.
-         *
-         * @return true/false depending if the sequence was completed
-         */
-        public boolean insert(int characterCode) {
-
-            this.sequence.add(characterCode);
-
-            Integer expectedAtPosition = this.expected.get(this.sequence.size() - 1);
-
-            if(!expectedAtPosition.equals(characterCode)) {
-                this.sequence.clear();
-                return false;
-            }
-
-            return this.sequence.size() == this.expected.size();
-        }
     }
 }
